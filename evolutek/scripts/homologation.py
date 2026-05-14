15ffc9936ffca045556e248046b686460b58b97f
PAL <git@pal>
2026-05-14 04:28:53 +0200

    config: Working config

--- evolutek/scripts/homologation.py
+++ evolutek/scripts/homologation.py
@@ -2,13 +2,16 @@ from evo_robot.ai.script import ScriptContext, script
 from evo_robot.trajman.trajman import TrajmanPeripheral
 
 from evo_lib.types.vect import Vect2D
-
-from .grab import main as grab
+from evo_lib.types.pose import Pose2D
 
 
 @script()
 def main(ctx: ScriptContext):
     trajman = ctx.peripheral("trajman", TrajmanPeripheral)
 
-    trajman.go_to(Vect2D(300, 1100)).wait()
-    grab(ctx, "front")
+    # trajman.free().wait()
+    # trajman.set_position(Pose2D(300, 1100, 0)).wait()
+    # trajman.unfree().wait()
+
+    trajman.go_to(Vect2D(900, 1100)).wait()
+    #grab(ctx, "front")
